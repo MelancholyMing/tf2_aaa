8c129d5b47c7967ec088feddf994c8df619d34a2
ming-win10-home <melancholyming@gmail.com>
2021-03-03 18:43:41 +0800

    "21/3/2 NG course kmean"

--- pycharmproject/NG_course/kmeans_image.py
+++ pycharmproject/NG_course/kmeans_image.py
@@ -108,13 +108,20 @@ def _k_means_iter(data, k, epoch=100, tol=0.0001):
 
 C, centroids, cost = k_means(pd.DataFrame(data), 16, epoch=10, n_init=3)
 
+compressed_pic = centroids[C].reshape((128, 128, 3))
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(pic)
+ax[1].imshow(compressed_pic)
+plt.show()
+
+
 from sklearn.cluster import KMeans
 
 model = KMeans(n_clusters=16, n_init=100, n_jobs=-1)
 
 model.fit(data)
 
-# %%
 
 centroids = model.cluster_centers_
 print(centroids.shape)

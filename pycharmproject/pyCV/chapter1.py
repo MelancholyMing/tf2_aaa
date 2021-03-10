01a456e59f307d0ad2a330f44c8cba8045d4d5c4
melancholyming <melancholyming@gmail.com>
2021-03-10 12:45:02 +0800

    'test'

--- pycharmproject/pyCV/chapter1.py
+++ pycharmproject/pyCV/chapter1.py
@@ -1,45 +1,29 @@
 import cv2
 
 # img
-# img = cv2.imread("./resources/len_std.jpg")
+# img = cv2.imread("./resources/len_std.png")
 #
-# cv2.imshow("output",img)
-# cv2.waitKey(0)
+# cv2.imshow("output", img)
+# cv2.waitKey(1000)
+# cv2.destroyAllWindows()
 
 # video
-# cap = cv2.VideoCapture("resources/test_video.mp4")
+# cap = cv2.VideoCapture("./resources/test_video.mp4")
 #
 # while True:
 #     success, img = cap.read()
 #     cv2.imshow('Video', img)
-#     if cv2.waitKey(1) & 0xff == ord('q'):
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
-cap.set(4, 480)
-cap.set(10,100)
+# cap.set(4, 480)
+# cap.set(10, 100)con
 
-"""
-channels:
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/pro/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
-  - defaults
-show_channel_urls: true
-default_channels:
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
-custom_channels:
-  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
-ssl_verify: true
-remote_read_timeout_secs: 1000.0
-"""
+while True:
+    success, img = cap.read()
+    cv2.imshow('Video', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
